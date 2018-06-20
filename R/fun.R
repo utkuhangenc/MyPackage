5d57553a969de3b1ea8d101c1d8c8df533248568
utkuhangenc <40195837+utkuhangenc@users.noreply.github.com>
2018-06-20 11:48:11 +0300

    Initial commit

---
+++ R/fun.R
@@ -0,0 +1,13 @@
+#' A Fun Function
+#'
+#' This function allows you to find square of every input.
+#' @param x Input
+#' @keywords x
+#' @export
+#' @examples 
+#' fun_function(5)
+
+
+fun_function <- function(x){x^2}
+
+
